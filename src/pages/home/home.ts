929ed3335039e0e392f11aa7c54673b2e98481bf
Maruti Hulikatti <maruti@Marutis-MacBook-Pro.local>
2016-10-24 23:21:57 +0530

    first commit

---
+++ src/pages/home/home.ts
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core';
+
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+  public selectedEmployees: any = [];
+  public employees: any = [];
+  public isSearchOn: any = false;
+  public myInput:any ="";
+  constructor(public navCtrl: NavController) {
+      this.selectedEmployees = ["me","you","and","us","family"];
+      this.employees = ["dummy","care","me","try","we"];
+  }
+  search(event) {
+    let q = this.myInput;
+    if (q.trim() == '') {
+      this.isSearchOn = false;
+      return;
+    }
+    this.isSearchOn = true;
+  }
+  onCancel(event) {
+    this.isSearchOn = false;
+  }
+  onBlur(event) {
+    this.myInput = "";
+    this.isSearchOn = false;
+
+  }
+  itemTapped(event, employee) {
+    debugger
+    this.isSearchOn = false;
+    console.log(employee);
+  }
+
+}
